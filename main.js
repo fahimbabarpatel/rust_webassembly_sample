c632ea7b11e2fbcba3377647493c2be599cba38a
Fahim Babar Patel <pafahim@gmail.com>
2021-03-23 20:33:06 +0530

    random integer generate

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as wasm from './pkg';
 
-let result = wasm.add(1,1);
+let result = wasm.add(Math.floor(Math.random() * 11), Math.floor(Math.random() * 11));
 
 console.log(result);
 document.write(result)
